661d64353b5f07171bfa157c8a8ee7477d8eb75a
Bhupendra Lute <bhupendralute1234@gmail.com>
2024-07-08 17:53:15 +0530

    initial commit

---
+++ readme.md
@@ -0,0 +1,23 @@
+# Job Portal Server
+
+This is the server-side component of a Job Portal application built using the MERN stack.
+
+## Table of Contents
+
+- [Installation](#installation)
+- [APIs](#apis)
+- [Usage](#usage)
+
+## Installation
+
+### Prerequisites
+
+- Node.js
+- MongoDB
+
+### Steps
+
+1. **Clone the repository:**
+
+   ```bash
+   git clone https://github.com/your-username/job-portal.git
---
+++ src/routes/job-posting.route.js
@@ -0,0 +1,18 @@
+import { Router } from "express";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+import { applyJob, createJobPost, getAllJobPosts, getApplicationsByJobId, getJobPost, removeJobPost, searchJobPosts, updateJobApplicationStatus, updateJobPost } from "../controllers/job-posting.controller.js";
+import { upload } from "../middlewares/multer.middleware.js";
+
+const router = Router()
+
+router.route('/create').post(verifyJWT, createJobPost)
+router.route('/update/:id').patch(verifyJWT, updateJobPost)
+router.route('/get/:id').get(verifyJWT, getJobPost)
+router.route('/get-all').get(verifyJWT, getAllJobPosts)
+router.route('/search').get(verifyJWT, searchJobPosts)
+router.route("/applications/:id").get(verifyJWT, getApplicationsByJobId)
+router.route('/delete/:id').delete(verifyJWT, removeJobPost)
+router.route('/apply/:id').patch(verifyJWT, upload.single("resume"), applyJob)
+router.route('/update-job-status/:id').patch(verifyJWT, updateJobApplicationStatus)
+
+export default router
